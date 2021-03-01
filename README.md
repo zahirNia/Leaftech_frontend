6b8c8e0f6c0317523936e2aa01979324bf371d61
Othmane <web@othmanemoustaouda.io>
2021-03-01 17:04:37 +0100

    Include PHPMailer via composer packet manager.

---
+++ README.md
@@ -0,0 +1,5 @@
+This repo contains the new Website files.
+It includes also a PHP mailer script.
+
+Requirements:
+  - PHP version > 7.0
---
+++ contactHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Payload example:
+ *
+ * name: Othmane
+ * Email: web@othmanemoustauda.io
+ * company: CoopSpace
+ * role: Energy commissioner
+ * newsletter: on
+ * SA: on
+ * BEF: on
+ * IP: on
+ * PV: on
+ * WPA: on
+ * message: Trying with Chrome
+ *
+ */
+
+$postData = $_POST;
+//var_dump($postData);
+
+//
